f89483b274bb829115acc551135a3eed38a7858f
Sofia <sofia.eoy.sp@hotmail.com>
2025-03-06 15:46:41 +0100

    Creating comments on my app from posts

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,63 @@
+class CommentsController < ApplicationController
+    before_action :set_post
+    before_action :authenticate_user!
+ 
+ 
+    def index
+        @comments = @post.comments
+    end
+
+    def show
+    end
+
+    def create
+        #@post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        @comment.user_id = current_user.id if current_user
+
+        if @comment.save
+            flash[:success] = "The comment has been saved."
+            redirect_to @post
+        else
+            flash[:danger] = "There was a problem saving the comment."
+        end
+    end
+
+    def update
+        #@post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            flash[:success] = "The comment has been updated."
+            redirect_to post_path(@post)
+        else
+            render 'edit'
+            flash[:danger] = "There was a problem updating the comment."
+        end
+    end
+
+    def edit
+        #@post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+
+    def destroy
+        #@post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        flash[:warning] = "The comment has been deleted."
+        redirect_to @post
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
+
+    def set_post
+        @post = Post.find(params[:post_id])
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'user_registrations/registrations'}
   resources :template_ones
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'index', to: 'template_ones#index'
+  root to: 'template_ones#index'
 
+  resources :posts do
+    resources :comments, except: [:index]
+  end
  # get "up" => "rails/health#show", as: :rails_health_check
 end
